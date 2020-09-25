7ee08219d9e9562e517cfa4a4417a5fa9bec9a2a
Chuck Lantz <clantz@microsoft.com>
2020-09-25 16:41:42 +0000

    Simplify

--- .devcontainer/build-image.sh
+++ .devcontainer/build-image.sh
@@ -4,4 +4,5 @@ PAT=$1
 USERNAME=${2:-"chuxel"}
 echo ${PAT} | echo docker login ghcr.io -u ${USERNAME} --password-stdin
 docker build -t ghcr.io/${USERNAME}/vscode -f base.Dockerfile .
+docker push ghcr.io/${USERNAME}/vscode
 
